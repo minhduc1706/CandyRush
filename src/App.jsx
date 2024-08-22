1f740fce8e8297d75e97117283f5dd514e6d0eee
minhduc1706 <minhduc0879395@gmail.com>
2024-08-22 11:59:16 +0700

    add based project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import blueCandy from "./images/blue-candy.png";
+import greenCandy from "./images/green-candy.png";
+import orangeCandy from "./images/orange-candy.png";
+import yellowCandy from "./images/yellow-candy.png";
+import redCandy from "./images/red-candy.png";
+import purpleCandy from "./images/purple-candy.png";
+import blank from "./images/blank.png";
+
+const WIDTH = 8;
+const candyColors = [
+  blueCandy,
+  greenCandy,
+  orangeCandy,
+  yellowCandy,
+  redCandy,
+  purpleCandy,
+];
+
+function App() {
+  return <>
+  </>;
+}
+
+export default App;
