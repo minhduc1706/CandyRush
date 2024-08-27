51fcac1062efe79a3217076bb0f195108f1b455a
minhduc1706 <minhduc0879395@gmail.com>
2024-08-27 11:00:56 +0700

    add drag and drop candy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setCandies, updateCandies } from "./features/candies/candiesActions";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -14,6 +14,8 @@ function App() {
   const candies = useSelector((state) => state.candies);
   const currentCandies = useSelector((state) => state.currentCandies);
   const score = useSelector((state) => state.score);
+  const [candyDragged, setCandyDragged] = useState(null);
+  const [candyToReplace, setCandyToReplace] = useState(null);
 
   const handleGameLogic = () => {
     const updatedCandies = [...candies];
@@ -21,6 +23,57 @@ function App() {
     checkForRows(3, updatedCandies, dispatch);
     dispatch(updateCandies(updatedCandies));
     moveIntoSquareBelow(updatedCandies);
+  };
+
+  const dragStart = (e) => {
+    setCandyDragged(e.target);
+  };
+
+  const dragDrop = (e) => {
+    setCandyToReplace(e.target);
+  };
+
+  const dragEnd = () => {
+    const candyDraggedIndex = parseInt(candyDragged.getAttribute("data-index"));
+    const candyToReplaceIndex = parseInt(
+      candyToReplace.getAttribute("data-index")
+    );
+
+  const rowStart = Math.floor(candyDraggedIndex / WIDTH) * WIDTH;
+  const rowEnd = rowStart + WIDTH - 1;
+
+    const validMoves = [
+      candyDraggedIndex - 1,
+      candyDraggedIndex - WIDTH,
+      candyDraggedIndex + WIDTH,
+    ];
+
+    if (candyDraggedIndex !== rowEnd) {
+      validMoves.push(candyDraggedIndex + 1);
+    }
+
+    console.log(candyDraggedIndex, candyToReplaceIndex);
+    const validMove = validMoves.includes(candyToReplaceIndex)
+
+    if (validMove) {
+      const updatedCandies = [...candies];
+      updatedCandies[candyToReplaceIndex] = candies[candyDraggedIndex];
+      updatedCandies[candyDraggedIndex] = candies[candyToReplaceIndex];
+  
+      dispatch(updateCandies(updatedCandies));
+  
+      checkForColumns(3, updatedCandies, dispatch);
+      checkForRows(3, updatedCandies, dispatch);
+      moveIntoSquareBelow(updatedCandies);
+  
+      setCandyDragged(null);
+      setCandyToReplace(null);
+    } else {
+      setCandyDragged(null);
+      setCandyToReplace(null);
+    }
+
+
   };
 
   const createBoard = () => {
@@ -55,8 +108,17 @@ function App() {
           <div
             className="relative w-[70px] h-[70px] cursor-pointer"
             key={index}
+            data-index={index}
+            data-src={candy.color}
+            draggable={true}
+            onDragStart={dragStart}
+            onDragOver={(e) => e.preventDefault()}
+            onDragEnter={(e) => e.preventDefault}
+            onDragLeave={(e) => e.preventDefault}
+            onDrop={dragDrop}
+            onDragEnd={dragEnd}
           >
-            <img src={candy.color} alt={candy.color} className="w-full" />
+            <img src={candy.color} alt={candy.color} className="w-full pointer-events-none" />
           </div>
         ))}
       </div>
